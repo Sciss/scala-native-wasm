3e7e8851c13c46fc88b3571f632476521e2aa55f
Hanns Holger Rutz <contact@sciss.de>
2019-01-12 18:14:54 +0100

    index2.html : plain page, just look at web console

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -2,9 +2,9 @@ import scala.scalanative.native.{CDouble, CInt}
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    println(s"Hello ${sys.env("USER")} from Scala Native in WebAssembly! ${intSqrt(49)}")
+    println(s"Hello ${sys.env("USER")} from Scala Native in WebAssembly!") // ${intSqrt(49)}")
   }
 
-  // XXX TODO --- how to make this appear with `-s EXPORTED_FUNCTIONS` ?
-  def intSqrt(x: CInt): CDouble = Math.sqrt(x)
+//   XXX TODO --- how to make this appear with `-s EXPORTED_FUNCTIONS` ?
+//  def intSqrt(x: CInt): CDouble = Math.sqrt(x)
 }
