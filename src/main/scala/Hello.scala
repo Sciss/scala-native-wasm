c27e9214d66491ad0889af804842fb82b8cb6e36
Hanns Holger Rutz <contact@sciss.de>
2019-01-12 18:01:54 +0100

    give up for today

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,6 +1,10 @@
-object Hello extends App {
+import scala.scalanative.native.{CDouble, CInt}
 
-  println("Hello from Scala Native in WebAssembly!")
+object Hello {
+  def main(args: Array[String]): Unit = {
+    println(s"Hello ${sys.env("USER")} from Scala Native in WebAssembly! ${intSqrt(49)}")
+  }
 
-  // def int_sqrt(x: CInt): CDouble = Math.sqrt(x)
+  // XXX TODO --- how to make this appear with `-s EXPORTED_FUNCTIONS` ?
+  def intSqrt(x: CInt): CDouble = Math.sqrt(x)
 }
